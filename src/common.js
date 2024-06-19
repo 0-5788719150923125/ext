8492e762fd3f75bad64adeb1f4f2f68b88323dc6
Luciferian Ink <LuciferianInk@protonmail.com>
2024-06-19 03:25:45 -0500

    Change some words

--- src/common.js
+++ src/common.js
@@ -29,7 +29,7 @@ export function eventHandler(event) {
         sendToForeground('toTopic', event.data.answer)
     } else if (event.data.status === 'partial') {
         sendToForeground('floatRight')
-        sendToForeground('toInputField', event.data.input + '//:fold')
+        sendToForeground('toInputField', event.data.input + '//:link')
     } else if (event.data.status === 'complete') {
         if (event.data.output.length > 2) {
             sendToForeground('toOutputField', event.data.output)
